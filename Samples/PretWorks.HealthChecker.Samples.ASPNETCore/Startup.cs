ce37052a6174fd81fa6c239ebf072074c059e33c
Rob van der Werf <r.vanderwerf@jex.nl>
2021-03-23 22:01:34 +0100

    .net 5 update

--- Samples/PretWorks.HealthChecker.Samples.ASPNETCore/Startup.cs
+++ Samples/PretWorks.HealthChecker.Samples.ASPNETCore/Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using PretWorks.HealthChecker.HealthCheckers;
 using PretWorks.HealthChecker.Middleware;
 
@@ -23,7 +24,7 @@ namespace PretWorks.HealthChecker.Samples.ASPNETCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -34,8 +35,6 @@ namespace PretWorks.HealthChecker.Samples.ASPNETCore
             {
                 settings.AddHealthChecker<BasicHealthChecker>();
             });
-
-            app.UseMvc();
         }
     }
 }
